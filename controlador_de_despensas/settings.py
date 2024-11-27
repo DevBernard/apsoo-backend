0c75240ae45d8c72d2f5e745065e9f92ba72937e
DevBernard <bernard.dev.mat@gmail.com>
2024-11-26 22:05:52 -0300

    access token for 32 years

--- controlador_de_despensas/settings.py
+++ controlador_de_despensas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,6 +152,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=32*365),
 }
 
 SPECTACULAR_SETTINGS = {
